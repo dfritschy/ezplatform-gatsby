aa3848b4fa7e6d2b79ce673d87598a2a00f9f957
dfritschy <donat@fritschy.ch>
2019-10-18 12:41:00 +0200

    Updated index page to query content from eZ repository

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,54 @@
 import React from "react"
-import { Link } from "gatsby"
+import {graphql} from "gatsby"
+import Layout from '../components/layout'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const assetsUrl = process.env.GATSBY_ASSETS_URL
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({data}) => (
+    <Layout>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        {data.ezRepository.netgen.ngArticles.edges.map(document => (
+            <article style={{marginBottom: `2rem`}}>
+                <header>
+                    <div><em>{document.node.tags}</em></div>
+                    <h2>{document.node.title}</h2>
+                </header>
+                <img src={assetsUrl + document.node.image.variation.uri} alt={document.node.title}/>
+                <div dangerouslySetInnerHTML={createMarkup(document.node.fullIntro)}/>
+            </article>
+        ))}
+    </Layout>
+);
 
-export default IndexPage
+export const query = graphql`
+{
+  ezRepository {
+    netgen {
+      ngArticles(last: 10, sortBy: _datePublished) {
+        edges {
+          node {
+            title
+            id
+            _url
+            fullIntro
+            tags
+            image {
+              variation(identifier: i480) {
+                uri
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+function createMarkup(snippet) {
+    return {__html: snippet};
+}
+
+export default IndexPage;
